a8a9281f8dc13d335f9524c7e6e21f5bdf267e03
Varun Upadhyay <varun.u28@gmail.com>
2017-11-16 10:07:00 -0800

    Added data cleaning module

---
+++ Code/data_cleaning.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+features = pd.read_csv('/home/varunu28/Desktop/predicting-dengue/datasets/dengue_features_train.csv')
+# print(features)
+
+labels = pd.read_csv('/home/varunu28/Desktop/predicting-dengue/datasets/dengue_labels_train.csv')
+# print(labels)
+
+features_iq = features[features['city'] == 'iq']
+features_sj = features[features['city'] == 'sj']
+
+# print(features_sj)
+# print(features_iq)
+
+features_iq_mean = features_iq.mean()
+features_sj_mean = features_sj.mean()
+
+# Cleaning the data
+features_sj = features_sj.fillna(features_sj_mean)
+features_iq = features_iq.fillna(features_iq_mean)
+
+# print(features_sj)
+# print(features_iq)
