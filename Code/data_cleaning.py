424c4cf3a0377c0a8133be1e50d373525674bf4c
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-11-16 11:43:28 -0800

    Added code for dateparser and non numeric data cleaning

--- Code/data_cleaning.py
+++ Code/data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-features = pd.read_csv('../data/dengue_features_train.csv')
+features = pd.read_csv('../data/dengue_features_train.csv', parse_dates=[3])
 # print(features)
 
 features_iq = features[features['city'] == 'iq']
@@ -20,8 +20,11 @@ features_iq = features_iq.fillna(features_iq_mean)
 # print(features_sj)
 # print(features_iq)
 
+# drop non-numerical values
+features_sj.drop(['city', 'year'], axis = 1, inplace = True)
+features_iq.drop(['city', 'year'], axis = 1, inplace = True)
+
 # Saving the cleaned data
 features_sj.to_csv("../data/features_sj.csv")
 features_iq.to_csv("../data/features_iq.csv")
-
 
