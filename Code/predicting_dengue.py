03d9569ec46fe9fb7f03c7701969112f1215e237
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-11-22 11:12:57 -0800

    ploting cases for week after standardizing the input

--- Code/predicting_dengue.py
+++ Code/predicting_dengue.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot
+from sklearn.preprocessing import StandardScaler
 
 
 feature_sj = pd.read_csv("../data/features_sj.csv")
@@ -91,4 +92,84 @@ pyplot.legend(loc = 'best')
 
 
 pyplot.tight_layout(pad=3)
+pyplot.show()
+
+#Standardizing temperature and rain units in SJ
+ss = StandardScaler()
+Xs = ss.fit_transform(feature_sj[feature_sj.columns[7:23]])
+feature_sj_s = pd.DataFrame(Xs, columns=feature_sj.columns[7:23], index=feature_sj.index)
+feature_sj_s['weekofyear'] = feature_sj['weekofyear']
+
+#Standardizing temperature and rain units in IQ
+Xs = ss.fit_transform(feature_iq[feature_iq.columns[7:23]])
+feature_iq_s = pd.DataFrame(Xs, columns = feature_iq.columns[7:23], index=feature_iq.index)
+feature_iq_s['weekofyear'] = feature_iq['weekofyear']
+
+#print(feature_sj_s.columns[:-1])
+#print(feature_iq_s.columns[:-1])
+
+
+#Plot all Normalized weather data in San Juan
+
+for i in (feature_sj_s.columns[:-1]):
+    feature_sj_s.groupby('weekofyear')[i].mean()\
+    .plot(alpha = .3, figsize = (22, 5))\
+    .legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# plt.legend(loc = 'best')
+pyplot.title('Standard Weather: San Juan, Puerto Rico', size = 25)
+pyplot.ylabel('Normal Scale', size = 20)
+pyplot.xlabel('Week of Year', size = 20)
+pyplot.show()
+
+
+#Plot all Normalized weather data in Iquitos
+for i in (feature_iq_s.columns[:-1]):
+    feature_iq_s.groupby('weekofyear')[i].mean()\
+    .plot(alpha = .3, figsize = (22, 5))\
+    .legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# plt.legend(loc = 'best')
+pyplot.title('Standard Weather : in Iquitos, Peru', size = 25)
+pyplot.ylabel('Normal Scale', size = 20)
+pyplot.xlabel('Week of Year', size = 20)
+pyplot.show()
+
+
+# create sj and iq targets df
+target_sj = target[target['city'] == 'sj']
+target_iq = target[target['city'] == 'iq']
+
+#Checking mean for both cities
+#print('San Jose  - Mean: {} and STD: {}'.format(target_sj['total_cases'].mean(), target_sj['total_cases'].std()))
+#print ('Iquitos  - Mean: {} and STD: {}'.format(target_iq['total_cases'].mean(), target_iq['total_cases'].std()))
+
+# plot cases for each week each year in san juan
+for i in set(target_sj['year']):
+    df = target_sj[target_sj['year'] == i]
+    df.set_index('weekofyear', drop=True, inplace=True)
+    pyplot.plot(df['total_cases'], alpha=.3)
+
+target_sj.groupby('weekofyear')['total_cases'].mean().plot(c='k', figsize=(10, 4))
+pyplot.legend(set(target_sj['year']), loc='center left', bbox_to_anchor=(1, .5))
+
+pyplot.title('Cases per Week : San Juan, Puerto Rico')
+pyplot.ylabel('Number of Cases')
+pyplot.xlabel('Week of the Year')
+
+pyplot.show()
+
+
+
+# plot cases for each week each year in iquitos
+for i in set(target_iq['year']):
+    df = target_iq[target_iq['year'] == i]
+    df.set_index('weekofyear', drop = True, inplace = True)
+    pyplot.plot(df['total_cases'], alpha = .3)
+
+target_iq.groupby('weekofyear')['total_cases'].mean().plot(c = 'k', figsize = (10,4))
+pyplot.legend(set(target_iq['year']), loc='center left', bbox_to_anchor=(1, .5))
+
+pyplot.title('Cases per Week : Iquitos, Peru')
+pyplot.ylabel('Number of Cases')
+pyplot.xlabel('Week of the Year')
+
 pyplot.show()
